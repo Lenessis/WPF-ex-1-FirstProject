107424bbc642199bd1de74b423c5f3c96dd9bc14
Lenessis <mkorecka@gmail.com>
2022-04-05 10:17:53 +0200

    Finished exercise

---
+++ Machine.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace zadanie1
+{
+    class Machine
+    {
+        private List<string> tokens;
+
+        public Machine()
+        {
+            tokens = new List<string>();
+        }
+
+        public List<string> ShowAll()
+        {
+            return tokens; //pokazywanie calej listy
+        }
+
+        public void AddToken(string token)
+        {
+            tokens.Add(token); //dodawanie kolejnego kuponu
+        }
+
+        public Boolean IsMachineEmpty()
+        {
+            // Czy maszyna jest pusta?
+            if (tokens.Count == 0)
+                return true;
+            else 
+                return false;
+        }
+
+        public string GetRandomToken()
+        {
+            //losowanie kuponu
+            Random rnd = new Random();
+            int index = rnd.Next(0, tokens.Count);
+            return tokens[index];
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace zadanie1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Machine m;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            m = new Machine();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //wyjmij
+            if (m.IsMachineEmpty() == false)
+                result.Content = m.GetRandomToken() + "\n -- Wykonano operacje GetRandomToken() --";
+            else
+                result.Content = "Brak słów w maszynie";
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+            //włóż
+            //m. type_here.Text
+            m.AddToken(type_here.Text);
+            result.Content = " -- Wykonano operacje AddToken() --";
+            ShowList();
+        }
+
+        private void ShowList()
+        {
+            all.Content = "Lista słów: ";
+            List<string> list = m.ShowAll();
+            foreach (var item in list)
+            {
+                all.Content += ("\n" + item);
+            }
+            all.Content +=" --- wykonano operacje ShowAll()";
+        }
+    }
+}
